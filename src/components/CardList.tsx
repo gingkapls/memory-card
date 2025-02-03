b3b78a3818e89ce1038ffa0a47bd5cd756768183
gin <73906888+gingkapls@users.noreply.github.com>
2025-02-03 13:36:02 +0530

    feat(cardlist): init component

---
+++ src/components/CardList.tsx
@@ -0,0 +1,64 @@
+import Card, { pokemonCard, searchResult } from "./Card";
+import useData from "../hooks/useData";
+import React, { Dispatch, SetStateAction, useState } from "react";
+import shuffle from "../lib/shuffle";
+
+const URL = "https://pokeapi.co/api/v2/";
+
+interface pokemonAPIResponse {
+  count: number;
+  results: searchResult[];
+  next: string;
+}
+
+interface CardListProps {
+  number: number;
+  score: number;
+  setScore: Dispatch<SetStateAction<number>>;
+  highScore: number;
+  setHighScore: Dispatch<SetStateAction<number>>;
+}
+
+function CardList({
+  number,
+  score,
+  setScore,
+  highScore,
+  setHighScore,
+}: CardListProps) {
+  const query = `pokemon?limit=${number}&offset=0`;
+  const response = useData<pokemonAPIResponse>(URL + query);
+  const [clickedCards, setClickedCards] = useState<string[]>([]);
+
+  if (!response) return <>Loading</>;
+
+  const cards: searchResult[] = shuffle<searchResult>(response.results);
+  console.log(cards);
+
+  function resetGame() {
+    const newHighScore = Math.max(score, highScore);
+    setHighScore(newHighScore);
+    setScore(0);
+    setClickedCards([]);
+  }
+
+  function clickHandler(name: string) {
+    if (clickedCards.includes(name)) {
+      resetGame();
+      return;
+    }
+
+    setClickedCards([...clickedCards, name]);
+    setScore(score + 1);
+  }
+
+  return (
+    <main>
+      {cards.map((card) => (
+        <Card key={card.name} {...card} onClick={() => clickHandler(card.name)} />
+      ))}
+    </main>
+  );
+}
+
+export default CardList;
