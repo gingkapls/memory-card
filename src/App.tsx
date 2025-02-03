a697746d766bb1bf9aacbf3fb97ae9c171ebd732
gin <73906888+gingkapls@users.noreply.github.com>
2025-02-03 13:36:16 +0530

    feat(app): add driver code and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import { useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import CardList from "./components/CardList";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [score, setScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  console.log("score: ", score);
+  console.log("high score: ", highScore);
   return (
     <>
+      <h1>Pokemon Memory game!</h1>
+      <h3>Score: {score}</h3>
+      <h3>High Score: {highScore}</h3>
+
+      <CardList
+        number={10}
+        score={score}
+        setScore={setScore}
+        highScore={highScore}
+        setHighScore={setHighScore}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
